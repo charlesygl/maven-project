1583db2c624b408d5ebae8926544deed3cc8ca7b
Gaole Yu <charlesyu@Gaoles-MacBook-Pro.local>
2018-04-23 22:44:34 -0400

    another fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * @param something
+   * @param someone
    * @return greeting string
    */
   public final String greet(final String someone) {
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -2,3 +2,4 @@
  * This is a package info
  */
  package com.example;
+ 
